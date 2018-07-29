ea1b6c3b7530a5f57b8da21817e229def04fa681
YangJie-Git <1231233@qq.com>
2018-07-29 23:15:45 +0800

    init commit

---
+++ common/tpls/banner3/banner3.js
@@ -0,0 +1,10 @@
+// common/tpls/banner3/banner3.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  
+  },
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,9 +6,28 @@ Page({
   data: {
     activeIndex: 0,
     sliderOffset: 0,
-    activeType:"home",
-    tabs: [
+    activeType: "home",
+    winWidth: 0,
+    winHeight: 0,
+    currentTab: 0,
+    banner3:[
       {
+        goid:4,
+        src: "../../images/def.jpg",
+        url:""
+      },
+      {
+        goid: 2,
+        src: "../../images/def.jpg",
+        url: ""
+      },
+      {
+        goid: 5,
+        src: "../../images/def.jpg",
+        url: ""
+      },
+    ],
+    tabs: [{
         'id': 1,
         'name': '精选',
         'type': 'home'
@@ -38,23 +57,79 @@ Page({
         'name': '更多',
         'type': 'goapp'
       }
+    ],
+    recommendeds:[
+      {
+        'id': 1,
+        'name': '推荐',
+        'scr': '../../images/icon_classify.png'
+      },
+      {
+        'id': 2,
+        'name': '推荐',
+        'scr': '../../images/icon_classify.png'
+      },
+      {
+        'id': 3,
+        'name': '推荐',
+        'scr': '../../images/icon_classify.png'
+      },
+      {
+        'id': 4,
+        'name': '推荐',
+        'scr': '../../images/icon_classify.png'
+      },
+      {
+        'id': 5,
+        'name': '推荐',
+        'scr': '../../images/icon_classify.png'
+      },
+      {
+        'id': 6,
+        'name': '推荐',
+        'scr': '../../images/icon_classify.png'
+      },
+      {
+        'id': 7,
+        'name': '推荐',
+        'scr': '../../images/icon_classify.png'
+      },
+      {
+        'id': 8,
+        'name': '推荐',
+        'scr': '../../images/icon_classify.png'
+      },
     ]
   },
 
-  onLoad: function () {
+  onLoad: function() {
     console.log("index.js onLoad");
     var that = this;
     wx.getSystemInfo({
-      success: function (res) {
+      success: function(res) {
         var data = {
+          winWidth: res.windowWidth,
+          winHeight: res.windowHeight,
           sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
         }
-        console.info("index.js onLoad getSystemInfo success", data,res);
+        console.info("index.js onLoad getSystemInfo success", data, res);
+        data.lists = [];
+        for(var i=0;i<10;i++){
+          data.lists.push(
+            {
+              id:i,
+              name:"节目",
+            }
+          );
+        }
         that.setData(data);
       }
     });
   },
-
+  getSliderOffset: function (activeIndex){
+    console.log("getSliderOffset", this.data.winWidth, this.data.tabs.length, activeIndex)
+    return Math.round(this.data.winWidth / this.data.tabs.length * activeIndex);
+  },
   /**
    * 页面显示
    */
@@ -68,29 +143,61 @@ Page({
   /**
    * tab点击处理
    */
-  tabClick: function (e) {
+  tabClick: function(e) {
     var idx = e.currentTarget.id;
     console.log("tabClick", e.currentTarget);
+    // if (this.data.currentTab === e.currentTarget.dataset.current) {
+    //   //点击的是同一个，则不操作
+    //   return false;
+    // } else {
+    //   that.setData({
+    //     currentTab: e.currentTarget.dataset.current
+    //   })
+    // }
     var data = {
       activeIndex: idx, // 当前点击的tab索引
-      sliderOffset: e.currentTarget.offsetLeft,
+      sliderOffset: this.getSliderOffset(idx),
       // type类型 home:主页 goapp:提示下载 '':其他
       activeType: this.data.tabs[idx].type ? this.data.tabs[idx].type : "", // 当前tab索引对应的类型
     };
     console.log("tabClick setData", data);
     this.setData(data);
   },
-  testClick: function (e) {
-    console.log("aaaaaaaaaaaa");
-    var idx =4;
+  /**
+   * 滑动切换tab
+   */
+  swiperChange: function(e) {
+    console.log("swiperChange",e);
+    var idx = e.detail.current;
+    var that = this;
+    that.setData({
+      activeIndex: idx,
+      sliderOffset: this.getSliderOffset(idx),
+      activeType: this.data.tabs[idx].type ? this.data.tabs[idx].type : "",
+    });
+  },
+  bannerClick: function(e) {
+    console.log("aaaaaaaaaaaa", e);
+    var idx = e.currentTarget.id;
     var data = {
       activeIndex: idx, // 当前点击的tab索引
-      sliderOffset: this.data.sliderWidthOffsetStep*idx,
+      sliderOffset: this.data.sliderWidthOffsetStep * idx,
       // type类型 home:主页 goapp:提示下载 '':其他
       activeType: this.data.tabs[idx].type ? this.data.tabs[idx].type : "", // 当前tab索引对应的类型
     };
     this.setData(data);
-  }
+  },
+  shareClick: function (e) {
+    console.log("aaaaaaaaaaaa", e);
+    var idx = e.currentTarget.id;
+    var data = {
+      activeIndex: idx, // 当前点击的tab索引
+      sliderOffset: this.data.sliderWidthOffsetStep * idx,
+      // type类型 home:主页 goapp:提示下载 '':其他
+      activeType: this.data.tabs[idx].type ? this.data.tabs[idx].type : "", // 当前tab索引对应的类型
+    };
+    this.setData(data);
+  },
 })
 
 // Page({
