ff1a87c5151303a2a0296b26bf36bc309c7c069e
YangJie-Git <1231233@qq.com>
2018-07-29 23:51:13 +0800

    init commit

--- pages/audio/audio.js
+++ pages/audio/audio.js
@@ -10,11 +10,11 @@ Page({
     winWidth: 0,
     winHeight: 0,
     currentTab: 0,
-    banner3:[
+    banner3: [
       {
-        goid:4,
+        goid: 4,
         src: "../../images/def.jpg",
-        url:""
+        url: ""
       },
       {
         goid: 2,
@@ -28,85 +28,75 @@ Page({
       },
     ],
     tabs: [{
-        'id': 1,
-        'name': '精选',
-        'type': 'home'
-      },
-      {
-        'id': 2,
-        'name': '动画'
-      },
-      {
-        'id': 3,
-        'name': '儿歌'
-      },
-      {
-        'id': 4,
-        'name': '故事'
-      },
-      {
-        'id': 5,
-        'name': '早教'
-      },
-      {
-        'id': 6,
-        'name': '英文'
-      },
-      {
-        'id': 7,
-        'name': '更多',
-        'type': 'goapp'
-      }
+      'id': 1,
+      'name': '精选',
+      'type': 'home'
+    },
+    {
+      'id': 2,
+      'name': '儿歌'
+    },
+    {
+      'id': 3,
+      'name': '故事'
+    },
+    {
+      'id': 4,
+      'name': '英文'
+    },
+    {
+      'id': 5,
+      'name': '国学'
+    },
+    {
+      'id': 6,
+      'name': '纯音乐'
+    },
+    {
+      'id': 7,
+      'name': '更多',
+      'type': 'goapp'
+    }
     ],
-    recommendeds:[
+    recommendeds: [
       {
         'id': 1,
-        'name': '汪汪立大功',
+        'name': '流行歌曲',
         'scr': '../../images/icon_classify.png'
       },
       {
         'id': 2,
-        'name': '宝狄与好友',
+        'name': '巴塔木儿歌',
         'scr': '../../images/icon_classify.png'
       },
       {
         'id': 3,
-        'name': '巴塔木英文歌',
+        'name': '睡前故事大全',
         'scr': '../../images/icon_classify.png'
       },
       {
         'id': 4,
-        'name': '互动手指谣',
+        'name': '性格培养故事',
         'scr': '../../images/icon_classify.png'
       },
       {
         'id': 5,
-        'name': '小猪佩奇',
+        'name': '三字经',
         'scr': '../../images/icon_classify.png'
       },
       {
         'id': 6,
         'name': '哈利讲故事',
         'scr': '../../images/icon_classify.png'
-      },
-      {
-        'id': 7,
-        'name': '怪怪拼音历险',
-        'scr': '../../images/icon_classify.png'
-      },
-      {
-        'id': 8,
-        'name': '思维大爆炸',
-        'scr': '../../images/icon_classify.png'
-      },
+      }
     ]
   },
 
-  onLoad: function() {
+  onLoad: function () {
     console.log("index.js onLoad");
     var that = this;
     wx.getSystemInfo({
-      success: function(res) {
+      success: function (res) {
         var data = {
           winWidth: res.windowWidth,
           winHeight: res.windowHeight,
@@ -114,11 +104,11 @@ Page({
         }
         console.info("index.js onLoad getSystemInfo success", data, res);
         data.lists = [];
-        for(var i=0;i<10;i++){
+        for (var i = 0; i < 10; i++) {
           data.lists.push(
             {
-              id:i,
-              name:"节目",
+              id: i,
+              name: "节目",
             }
           );
         }
@@ -126,7 +116,7 @@ Page({
       }
     });
   },
-  getSliderOffset: function (activeIndex){
+  getSliderOffset: function (activeIndex) {
     console.log("getSliderOffset", this.data.winWidth, this.data.tabs.length, activeIndex)
     return Math.round(this.data.winWidth / this.data.tabs.length * activeIndex);
   },
@@ -143,7 +133,7 @@ Page({
   /**
    * tab点击处理
    */
-  tabClick: function(e) {
+  tabClick: function (e) {
     var idx = e.currentTarget.id;
     console.log("tabClick", e.currentTarget);
     // if (this.data.currentTab === e.currentTarget.dataset.current) {
@@ -166,8 +156,8 @@ Page({
   /**
    * 滑动切换tab
    */
-  swiperChange: function(e) {
-    console.log("swiperChange",e);
+  swiperChange: function (e) {
+    console.log("swiperChange", e);
     var idx = e.detail.current;
     var that = this;
     that.setData({
@@ -176,7 +166,7 @@ Page({
       activeType: this.data.tabs[idx].type ? this.data.tabs[idx].type : "",
     });
   },
-  bannerClick: function(e) {
+  bannerClick: function (e) {
     console.log("aaaaaaaaaaaa", e);
     var idx = e.currentTarget.id;
     var data = {
@@ -199,54 +189,3 @@ Page({
     this.setData(data);
   },
 })
-
-// Page({
-//   data: {
-//     motto: 'Hello World',
-//     userInfo: {},
-//     hasUserInfo: false,
-//     canIUse: wx.canIUse('button.open-type.getUserInfo')
-//   },
-//   //事件处理函数
-//   bindViewTap: function() {
-//     wx.navigateTo({
-//       url: '../logs/logs'
-//     })
-//   },
-//   onLoad: function () {
-//     if (app.globalData.userInfo) {
-//       this.setData({
-//         userInfo: app.globalData.userInfo,
-//         hasUserInfo: true
-//       })
-//     } else if (this.data.canIUse){
-//       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-//       // 所以此处加入 callback 以防止这种情况
-//       app.userInfoReadyCallback = res => {
-//         this.setData({
-//           userInfo: res.userInfo,
-//           hasUserInfo: true
-//         })
-//       }
-//     } else {
-//       // 在没有 open-type=getUserInfo 版本的兼容处理
-//       wx.getUserInfo({
-//         success: res => {
-//           app.globalData.userInfo = res.userInfo
-//           this.setData({
-//             userInfo: res.userInfo,
-//             hasUserInfo: true
-//           })
-//         }
-//       })
-//     }
-//   },
-//   getUserInfo: function(e) {
-//     console.log(e)
-//     app.globalData.userInfo = e.detail.userInfo
-//     this.setData({
-//       userInfo: e.detail.userInfo,
-//       hasUserInfo: true
-//     })
-//   }
-// })
